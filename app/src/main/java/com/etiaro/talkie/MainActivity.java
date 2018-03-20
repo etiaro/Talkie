80a11aec0adbcb2a12e2848fbcd01eec99c54fb7
Etiaro <etianoo@gmail.com>
2018-03-20 20:47:44 +0100

    showing last 20 messages after login

--- app/src/main/java/com/etiaro/talkie/MainActivity.java
+++ app/src/main/java/com/etiaro/talkie/MainActivity.java
@@ -8,16 +8,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.JsonWriter;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.etiaro.facebook.Account;
 import com.etiaro.facebook.Facebook;
 import com.etiaro.facebook.Interfaces;
+import com.etiaro.facebook.functions.GetThreadList;
 import com.etiaro.facebook.functions.GetUserInfo;
 import com.etiaro.facebook.functions.Login;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -42,13 +48,41 @@ public class MainActivity extends AppCompatActivity {
         } else //No accounts, give login form
             runLoginActv();
 
-        for(String id : Facebook.getInstance().accounts.keySet()) {
-            Log.d("talkie", "getting user info "+id);
-            GetUserInfo ui = new GetUserInfo(Facebook.getInstance().accounts.get(id), id);
-            ui.execute();
-        }
-
         setContentView(R.layout.activity_main);
+
+        for(String id : Facebook.getInstance().accounts.keySet()) {
+            setTitle(Facebook.getInstance().accounts.get(id).getName());
+            GetThreadList ui = new GetThreadList(Facebook.getInstance().accounts.get(id), 20, System.currentTimeMillis(), new String[]{"INBOX"});
+
+            ui.execute(new Interfaces.ThreadListCallback() {
+                @Override
+                public void success(JSONArray list) {
+                    ArrayList<String> arr = new ArrayList<>();
+                    for(int i = 0; i < list.length(); i++){
+                        try {
+                            arr.add(list.getJSONObject(i).getJSONObject("last_message").getJSONArray("nodes")
+                                    .getJSONObject(0).getString("snippet"));
+                            Log.d("msg", list.getJSONObject(i).getJSONObject("last_message").getJSONArray("nodes")
+                                    .getJSONObject(0).getString("snippet"));
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1, arr);
+                    ((ListView)findViewById(R.id.msgList)).setAdapter(arrayAdapter);
+                }
+
+                @Override
+                public void fail() {
+
+                }
+
+                @Override
+                public void cancelled() {
+
+                }
+            });
+        }
     }
 
 
@@ -73,7 +107,6 @@ public class MainActivity extends AppCompatActivity {
             for(int i = 0; i < obj.length(); i++){
                 SharedPreferences sp = getSharedPreferences(getString(R.string.shared_pref_codes)+"."+obj.getString(i), Context.MODE_PRIVATE);
                 String JSON = sp.getString(getString(R.string.sp_account), null);
-                Log.d("json", JSON+"\n from -- "+getString(R.string.shared_pref_codes)+"."+obj.getString(i));
                 if(JSON == null){
                     continue;
                 }
@@ -82,9 +115,8 @@ public class MainActivity extends AppCompatActivity {
             }
         } catch (JSONException e) {
             Log.e("Talkie", "Critical error while parsing accounts data");
-            //TODO relog all
         }
-        saveAccIDs(); //to not
+        saveAccIDs(); //to delete failed accounts
 
         Log.d("talkie", "loaded accounts data");
     }
@@ -94,5 +126,5 @@ public class MainActivity extends AppCompatActivity {
         sp.edit().putString(getString(R.string.sp_account), ac.toString()).apply();
         Log.d("talkie", "Saved account data to "+getString(R.string.shared_pref_codes)+"."+ac.getUserID());
     }
-    //TODO Async Memory Manager
+    //TODO async Memory Manager
 }
